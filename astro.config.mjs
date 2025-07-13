ae45f542e3bde98ec14e7d5712e1b94689458cfa
Said Avendaño <saidandresmix01@gmail.com>
2025-07-13 13:25:55 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
-import node from '@astrojs/node'
-import 'dotenv/config'
-
+import netlify from '@astrojs/netlify/functions'
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  integrations: [vue()],
-  server: {
-    host: '0.0.0.0' // ✅ Esta línea es clave
-  },
-  vite: {
-    define: {
-      'import.meta.env.PUBLIC_API_URL': JSON.stringify(
-        process.env.PUBLIC_API_URL
-      )
-    }
-  }
+  output: 'server', // Astro SSR
+  adapter: netlify(),
+  integrations: [vue()]
 })
