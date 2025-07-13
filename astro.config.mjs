8df24d755aca7f50f7debb1912afcb3d7d88de6a
Said Avendaño <saidandresmix01@gmail.com>
2025-07-13 12:41:03 -0500

    add ebv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import node from '@astrojs/node';
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import node from '@astrojs/node'
 
 export default defineConfig({
   output: 'server',
@@ -8,5 +8,12 @@ export default defineConfig({
   integrations: [vue()],
   server: {
     host: '0.0.0.0' // ✅ Esta línea es clave
+  },
+  vite: {
+    define: {
+      'import.meta.env.PUBLIC_API_URL': JSON.stringify(
+        process.env.PUBLIC_API_URL
+      )
+    }
   }
-});
+})
